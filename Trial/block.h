fdf5cfd1b4d1a84f61e7811d55e7f6fc4db7d656
Drishya Karki <videosforlife1727@gmail.com>
2026-02-15 13:18:12 +0545

    Added block.h for the big block creation and postion.h and position.cpp for map(key-value pair)

---
+++ Trial/block.h
@@ -0,0 +1,9 @@
+//basically this file is the file which contains the outer blocks of the main inside blocks(more in the presentaion)
+
+#pragma once
+class Block{ //Block made for inheritance of other 8 big blocks
+    public:
+    Block();
+    int id;
+    private:
+};
